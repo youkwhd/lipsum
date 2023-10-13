71814af1f61416ff2008f5d13256466aa66e5045
youkwhd <lolywk@tutanota.com>
2023-10-13 21:43:28 +0700

    refactor: ignore errors, consider replacing clap

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,20 +1,22 @@
-use clap::{Command, Arg, ArgAction};
+use clap::{command, value_parser, Command, Arg, ArgAction};
+use std::env;
 use lipsum;
 
 fn parse_command_line_arguments() -> Command {
-    Command::new("lipsum")
+    command!()
         .about("lorem ipsum generator")
-        .arg(Arg::new("length")
-                .value_parser(clap::value_parser!(u32))
+        .ignore_errors(true)
+        .arg(Arg::new("__ARG_LENGTH")
+                .value_parser(value_parser!(u32))
                 .short('n')
                 .action(ArgAction::Set))
 }
 
 fn main() {
     let args = parse_command_line_arguments().get_matches();
-    let n_word = match args.try_get_one::<u32>("length") {
+    let n_word = match args.try_get_one::<u32>("__ARG_LENGTH") {
         Ok(x) => *x.unwrap_or(&10),
-        Err(_) => panic!("??????"),
+        Err(_) => unreachable!(),
     };
 
     println!("{}", lipsum::generate(n_word));
