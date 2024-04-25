acf67bcbe0f06c5d29c155e394c58e882c742954
youkwhd <lolywk@tutanota.com>
2024-04-25 14:42:22 +0700

    refactor: remove pico-args, simpler argument parsing

--- src/args.rs
+++ src/args.rs
@@ -1,36 +1,28 @@
-use pico_args;
-
 pub struct Args {
     pub words_length: u32,
-    pub paragraph_length: u32,
 }
 
 impl Args {
     fn print_help_and_exit(exit_code: i32) {
         println!(
 "\
-Usage: lipsum [-np]
-Lorem Ipsum generator.
-
-Options:
-  -n NUMBER       n words to generate
-  -p NUMBER       n paragraph to generate");
+Usage: lipsum <NWORDS>
+Lorem Ipsum generator.");
 
         std::process::exit(exit_code);
     }
 
-    pub fn parse() -> Result<Args, pico_args::Error> {
-        let mut pargs = pico_args::Arguments::from_env();
+    pub fn parse() -> Result<Args, std::io::Error> {
+        let args: Vec<String> = std::env::args().collect();
 
-        if pargs.contains(["-h", "--help"]) {
+        if args.contains(&String::from("-h")) || args.contains(&String::from("--help")) {
             Args::print_help_and_exit(0);
         }
 
-        let args = Args {
-            words_length: pargs.value_from_str("-n").or(Ok(10))?,
-            paragraph_length: pargs.value_from_str("-p").or(Ok(1))?,
-        };
+        if args.len() <= 1 {
+            Args::print_help_and_exit(1);
+        }
 
-        Ok(args)
+        Ok(Args { words_length: args[1].parse::<u32>().unwrap_or(10) })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,7 @@ mod args;
 use args::Args;
 
 fn main() {
-    let args = match Args::parse() {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("ERROR: {}.", e);
-            std::process::exit(1);
-        }
-    };
-
+    let args = Args::parse().unwrap();
     let lorem_ipsum = lipsum::generate(args.words_length);
     println!("{}", lorem_ipsum);
 }
